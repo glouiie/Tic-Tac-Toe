7fa488b8f24f8300155984f8d0fa18aac203c035
Glouiie <louiebeatty2020@gmail.com>
2023-07-11 13:16:31 +0100

    Added placement of naughts and crosses

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const htmlGameBoard = document.getElementById("game-board");
+
 
 const GameBoard = (() => {
 
   const startingBoard = 
   ["","","","","","","","",""]
+const htmlGameBoard = document.getElementById("game-board");
+
 
   function createGameBoard(){
 
@@ -22,5 +24,43 @@ const GameBoard = (() => {
 })();
 
 
+const gameController = (() => {
+
+  let marker = "naught"
+  const cellElement = document.getElementsByClassName("item")
+
+  function handleCellClick(event) {
+    const naughtOrCrossDisplay = document.createElement("div");
+    naughtOrCrossDisplay.classList.add(marker);
+    event.target.appendChild(naughtOrCrossDisplay);
+
+    if (marker === "naught") {
+      marker = "cross";
+    } else {
+      marker = "naught";
+    }
+
+    const clickedSquare = event.target;
+  }
+
+  function attachEventListeners() {
+    Array.from(cellElement).forEach((cell) => {
+      cell.addEventListener("click", handleCellClick);
+    });
+  }
+  return attachEventListeners()
+    
+})()
 
 
+
+
+function createPlayer(name,marker){
+
+  return{
+    name:name,
+    marker:marker,
+    score: 0,
+  }
+
+}
